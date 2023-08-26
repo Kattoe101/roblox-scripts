bbdf1feebec23318d6c1ebb3a1a03228624af171
Kattoe <116949619+Kattoe101@users.noreply.github.com>
2023-08-27 00:44:34 +0200

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,3 +1,3 @@
-if id == 734159876 then --sharkbite
+if game.gameid == 734159876 then --sharkbite
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Kattoe101/roblox-scripts/main/sharkbite.lua?token=GHSAT0AAAAAACGKVUGXQIMPVXVPHTYV55AKZHKNBSQ'))()
 end
