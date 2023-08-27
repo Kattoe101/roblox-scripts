0387ae0f29c703360a60fd44205f676d8b311b41
Kattoe <116949619+Kattoe101@users.noreply.github.com>
2023-08-27 14:14:15 +0200

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,3 +1,3 @@
-if (tonumber(game.PlaceId) == 734159876 then --sharkbite
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/Kattoe101/roblox-scripts/main/sharkbite.lua?token=GHSAT0AAAAAACGKVUGXQIMPVXVPHTYV55AKZHKNBSQ'))()
+if game.PlaceId == 734159876 then --sharkbite
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Kattoe101/roblox-scripts/main/sharkbite.lua'))()
 end
