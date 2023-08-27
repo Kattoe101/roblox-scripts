958a6a52dca2a793fbc03e42c16d26a820f5ed41
Kattoe <116949619+Kattoe101@users.noreply.github.com>
2023-08-28 01:55:00 +0200

    Update *hub.lua

--- *hub.lua
+++ *hub.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 734159876 then --sharkbite
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Kattoe101/roblox-scripts/main/sharkbite.lua'))()
 end
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
